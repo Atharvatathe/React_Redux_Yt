a809bd929c9b2c3b403c59ca3f04b41a88948711
Atharvatathe <atharva.tathe55@gmail.com>
2025-03-20 09:13:11 +0530

    create Todo component

--- src/store.jsx
+++ src/store.jsx
@@ -32,22 +32,12 @@ const taskReducer = (state = initialState, action) => {
 };
 
 // step 2: create the redux store using the reducer
-const store = createStore(taskReducer);
+export const store = createStore(taskReducer);
 
 //step 3: Log the initial state
 // The getState method is synchronous function that returns the current state of redux application. It include the entire state of the application, including akk the reducers and their respective state.
 
 console.log("initial state:", store.getState());
-
-//step 4: Dispatch action to add task
-store.dispatch(addTask("Buy Apple"));
-console.log("updated state:", store.getState());
-
-store.dispatch(addTask("Buy Mango"));
-console.log("updated state:", store.getState());
-
-store.dispatch(deleteTask(0));
-console.log("deleted state:", store.getState());
 
 // step 5: create action creators
 
@@ -58,3 +48,13 @@ const addTask = (data) => {
 const deleteTask = (data) => {
   store.dispatch({ type: DELETE_TASK, payload: data });
 };
+
+//step 4: Dispatch action to add task
+store.dispatch(addTask("Buy Apple"));
+console.log("updated state:", store.getState());
+
+store.dispatch(addTask("Buy Mango"));
+console.log("updated state:", store.getState());
+
+store.dispatch(deleteTask(0));
+console.log("deleted state:", store.getState());
