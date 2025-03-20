3310d7c2943076c74fc5662ab993b9fdcf190ea9
Atharvatathe <atharva.tathe55@gmail.com>
2025-03-20 07:58:10 +0530

    Create reducer function

---
+++ src/store.jsx
@@ -0,0 +1,28 @@
+const ADD_TASK = "task/add";
+const DELETE_TASK = "task/delete";
+
+const initialState = {
+  task: [],
+};
+
+const taskReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TASK:
+      return {
+        ...state,
+        task: [...state.task, action.payload],
+      };
+
+    case DELETE_TASK:
+      const updatedTask = state.task.filter((item, index) => {
+        return index !== action.payload;
+      });
+      return {
+        ...state,
+        task: [...state.task, updatedTask],
+      };
+
+    default:
+      return state;
+  }
+};
